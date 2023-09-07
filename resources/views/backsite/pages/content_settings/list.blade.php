0129123de5ae813b7f4c5521f057e150397f6ed2
David Pakpahan <davidpakpahan.developer@gmail.com>
2023-09-07 19:00:15 +0700

    test push

--- resources/views/backsite/pages/content_settings/list.blade.php
+++ resources/views/backsite/pages/content_settings/list.blade.php
@@ -52,7 +52,7 @@
                                     <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                                 </div>
                                 <div class="modal-body">
-                                    Are you sure you want to delete this data?
+                                    Are you sure you want to delete this item?
                                 </div>
                                 <div class="modal-footer">
                                     <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
